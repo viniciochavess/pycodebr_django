ff45f20b0d8e421c1833930f615f1762bb7f1736
viniciochavess <viniciochavess@gmail.com>
2024-04-29 21:52:52 -0300

    ForeignKey Car for brand

--- cars/admin.py
+++ cars/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from cars.models import Car
+from cars.models import Brand
 # Register your models here.
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model','brand','factory_year','model_year','value')
     search_fields = ('model',)
 
 admin.site.register(Car,CarAdmin)
+
+
+class BrandCarAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Brand,BrandCarAdmin)
