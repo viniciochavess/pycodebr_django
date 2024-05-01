31cb3d8a2843171622017f730a684c847f2e8615
viniciochavess <viniciochavess@gmail.com>
2024-05-01 19:21:36 -0300

    view car

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+def cars_view(request):
+    cars = Car.objects.all().order_by('model')
+    search = (request.GET.get('search'))
+    if search:
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
+    print(cars)
+    
+    return render(
+        request,
+        'cars.html',
+        {'cars':cars}
+    )
